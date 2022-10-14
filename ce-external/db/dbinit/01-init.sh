e3ef0e7280d7478bc3f885af2fb79d5af2c74294
yul <aymeric.guth@protonmail.com>
2022-10-15 00:52:37 +0200

    added postgres, pgadmin, kafka, zookeper

---
+++ ce-external/db/dbinit/01-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE authentication;
+  GRANT ALL PRIVILEGES ON DATABASE authentication TO $POSTGRES_USER;
+
+EOSQL
