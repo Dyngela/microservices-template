ee6306ed3d5d9ae3225114d33e3dcd758f712638
yul <aymeric.guth@protonmail.com>
2022-10-15 01:18:31 +0200

    shbang init.sh

--- ce-external/db/dbinit/01-init.sh
+++ ce-external/db/dbinit/01-init.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
-export PGPASSWORD=$POSTGRES_PASSWORD;
+export PGPASSWORD="$POSTGRES_PASSWORD";
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE authentication;
   GRANT ALL PRIVILEGES ON DATABASE authentication TO $POSTGRES_USER;
