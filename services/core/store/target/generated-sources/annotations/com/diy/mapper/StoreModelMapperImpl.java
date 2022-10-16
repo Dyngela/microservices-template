62ae19f793dc0c25836ce8e0328409fe0096be7c
Erwan Nutz <erwan.nutz@free.fr>
2022-10-16 12:51:52 +0200

    regulating modules build

--- services/core/store/target/generated-sources/annotations/com/diy/mapper/StoreModelMapperImpl.java
+++ services/core/store/target/generated-sources/annotations/com/diy/mapper/StoreModelMapperImpl.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-16T12:18:34+0200",
-    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.4.1 (OpenJDK Porters Group)"
+    date = "2022-10-16T12:50:48+0200",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.4 (Oracle Corporation)"
 )
 @Component
 public class StoreModelMapperImpl implements StoreModelMapper {
