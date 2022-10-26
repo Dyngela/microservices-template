4732f5e65457e9d4ab27025b003d8fe0edc84aca
yul <aymeric.guth@protonmail.com>
2022-10-26 21:25:51 +0200

    comment @Configuration core/notification

--- services/core/notification/src/main/java/com/diy/NotificationApplication.java
+++ services/core/notification/src/main/java/com/diy/NotificationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
+//@Configuration
 @EnableDiscoveryClient
 public class NotificationApplication {
     public static void main(String[] args) {
