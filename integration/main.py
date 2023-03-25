5b3557e2d14e5b3d7c7b0a48a1bdd5e651656834
yul <aymeric.guth@protonmail.com>
2023-03-25 13:42:04 +0100

    integration

--- integration/main.py
+++ integration/main.py
@@ -36,72 +36,72 @@ while 1:
 # healthcheck("store/all")
 ### création compte
 # create ADMIN user
-payload = {
-    "email": "azerty@gmail.com",
-    "password": "passeeeword",
-    "firstName": "ademeeine",
-    "lastName": "lasteeeNamee",
-    "phoneNumber": "+33 7 70 17 98 11",
-    "ethAddress": "0x000000",
-    "storeId": 1,
-    "role": "ADMIN",
-    "siret": "89e0987ee8e098790",
-    "storeName": "aeeez",
-    "sector": "updeate",
-    "subscriptionId": 1,
-}
+# payload = {
+#    "email": "azerty@gmail.com",
+#    "password": "passeeeword",
+#    "firstName": "ademeeine",
+#    "lastName": "lasteeeNamee",
+#    "phoneNumber": "+33 7 70 17 98 11",
+#    "ethAddress": "0x000000",
+#    "storeId": 1,
+#    "role": "ADMIN",
+#    "siret": "89e0987ee8e098790",
+#    "storeName": "aeeez",
+#    "sector": "updeate",
+#    "subscriptionId": 1,
+# }
 
-resp = requests.put(f"{BASE_URL}/api/v1/authentication/save", json=payload)
-msg = "{}: /api/v1/authentication/save"
-assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
-print(msg.format(SUCCESS))
-#
+# resp = requests.put(f"{BASE_URL}/api/v1/authentication/save", json=payload)
+# msg = "{}: /api/v1/authentication/save"
+# assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
+# print(msg.format(SUCCESS))
+##
 
-payload = {
-    "email": "admeieene@gmail.com",
-    "password": "aedmein",
-    "firstName": "ademeeine",
-    "lastName": "lasteeeNamee",
-    "phoneNumber": "+33 7 70 17 98 11",
-    "ethAddress": "0x000000",
-    "storeId": 1,
-    "role": "USER",
-    "siret": "89e0987eee8e098790",
-    "storeName": "aeeez",
-    "sector": "updeate",
-    "subscriptionId": 1,
-}
-route = "/api/v1/authentication/save"
-resp = requests.put(f"{BASE_URL}{route}", json=payload)
-msg = "{}: {}"
-assert resp.status_code == 200, (
-    msg.format(FAILED, route) + "\n" + resp.content.decode("utf-8")
-)
-print(msg.format(SUCCESS, route))
-
-
-### login
-resp = requests.post(
-    f"{BASE_URL}/api/v1/authentication/login",
-    json={
-        "email": "azerty@gmail.com",
-        "password": "passeeeword",
-    },
-)
-msg = "{}: /api/v1/authentication/login"
-assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
-print(msg.format(SUCCESS))
-token = resp.content.decode("utf-8")
+# payload = {
+#    "email": "admeieene@gmail.com",
+#    "password": "aedmein",
+#    "firstName": "ademeeine",
+#    "lastName": "lasteeeNamee",
+#    "phoneNumber": "+33 7 70 17 98 11",
+#    "ethAddress": "0x000000",
+#    "storeId": 1,
+#    "role": "USER",
+#    "siret": "89e0987eee8e098790",
+#    "storeName": "aeeez",
+#    "sector": "updeate",
+#    "subscriptionId": 1,
+# }
+# route = "/api/v1/authentication/save"
+# resp = requests.put(f"{BASE_URL}{route}", json=payload)
+# msg = "{}: {}"
+# assert resp.status_code == 200, (
+#    msg.format(FAILED, route) + "\n" + resp.content.decode("utf-8")
+# )
+# print(msg.format(SUCCESS, route))
 
 
-# list all stores
-resp = requests.get(f"{BASE_URL}/api/v1/store/all", headers={"Authorization": token})
-msg = "{}: /api/v1/store/all"
-assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
-print(msg.format(SUCCESS))
+#### login
+# resp = requests.post(
+#    f"{BASE_URL}/api/v1/authentication/login",
+#    json={
+#        "email": "azerty@gmail.com",
+#        "password": "passeeeword",
+#    },
+# )
+# msg = "{}: /api/v1/authentication/login"
+# assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
+# print(msg.format(SUCCESS))
+# token = resp.content.decode("utf-8")
 
 
-# list all products from store 1
-resp = requests.get(f"{BASE_URL}/api/v1/product/product/all/1")
-assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
-print(msg.format(SUCCESS))
+## list all stores
+# resp = requests.get(f"{BASE_URL}/api/v1/store/all", headers={"Authorization": token})
+# msg = "{}: /api/v1/store/all"
+# assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
+# print(msg.format(SUCCESS))
+
+
+## list all products from store 1
+# resp = requests.get(f"{BASE_URL}/api/v1/product/product/all/1")
+# assert resp.status_code == 200, msg.format(FAILED) + "\n" + resp.content.decode("utf-8")
+# print(msg.format(SUCCESS))
